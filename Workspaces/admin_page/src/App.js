1b95cac87c2759930eaaa676d6e298007f715954
MIG58 <michaelg17801@gmail.com>
2024-01-17 21:37:32 +0530

    admin

---
+++ Workspaces/admin_page/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Admin from './Components/Admin';
+// import filename from location
+
+function App() {
+  return (
+    <div className="App">
+      {/* Normal Function */}
+      {/* function call */}
+      {/* <First/> */}
+      <Admin/>
+    </div>
+    
+  );
+}
+// function First(){
+
+// }
+export default App;
+
---
+++ Workspaces/admin_page/src/Components/Admin.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+const Admin = () => { // Arrow Function
+    return(
+        <>
+        <div className="exm">
+            <p>Hello World</p>
+        </div>
+        <div className="container-fluid ">
+            <div className="row">
+                <div className="exm1 col-lg-12 col-md-6 col-sm-1">
+
+                </div>
+            </div>
+        </div>
+
+        {/* All Website Code */}
+        </>
+    )
+}
+export default Admin
+
+
+
+
