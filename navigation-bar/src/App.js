7739dc2653ec339b981f53ddb802ed2b48d87d7a
MIG58 <michaelg17801@gmail.com>
2024-01-30 22:24:31 +0530

    Navbar color

---
+++ navigation-bar/src/App.js
@@ -0,0 +1,24 @@
+import Navbar from './Navigation/Navbar';
+import Footer from './Footer/Footer';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
